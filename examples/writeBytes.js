2e539b948836897385606235a8774aa59ad0466c
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2012-04-28 19:43:56 +0200

    Updated examples

--- examples/writeBytes.js
+++ examples/writeBytes.js
@@ -1,17 +1,25 @@
 var BufferedWriter = require ("../build/buffered-writer");
 
 //The BufferedWriter truncates the file because append == false
-new BufferedWriter ("file").on ("error", function (error){
-	console.log (error);
-})
+new BufferedWriter ("file")
+	.on ("error", function (error){
+		console.log (error);
+	})
+	
 	//From the beginning of the file:
 	.write ([0x00, 0x01, 0x02], 0, 3) //Writes 0x00, 0x01, 0x02
 	.write (new Buffer ([0x03, 0x04]), 1, 1) //Writes 0x04
 	.write (0x05) //Writes 0x05
-	.close ();
+	.close (); //Closes the writer. A flush is implicitly done.
 
 //The BufferedWriter appends content to the end of the file because append == true
-//From the end of the file:
-new BufferedWriter ("file", true).write (0xFF).close (); //Writes 0xFF
+new BufferedWriter ("file", true)
+	.on ("error", function (error){
+		console.log (error);
+	})
+	
+	//From the end of the file:
+	.write (0xFF) //Writes 0xFF
+	.close (); //Closes the writer. A flush is implicitly done.
 
 //The file contains: 0x00, 0x01, 0x02, 0x04, 0x05, 0xFF
--- examples/writeText.js
+++ examples/writeText.js
@@ -2,25 +2,26 @@ var BufferedWriter = require ("../build/buffered-writer");
 
 //The BufferedWriter truncates the file because append == false
 new BufferedWriter ("file", "utf8")
+	.on ("error", function (error){
+		console.log (error);
+	})
+	
 	//From the beginning of the file:
-	//Writes "First line"
-	.write ("First line")
-	//Writes EOL (OS dependent; \r\n on Windows, otherwise \n)
-	.newLine ()
-	//Writes "Second line"
-	.write ("Second line")
-	//Closes the writer. A flush is implicitly done.
-	.close ();
+	.write ("First line") //Writes "First line"
+	.newLine () //Writes EOL (OS dependent; \r\n on Windows, otherwise \n)
+	.write ("Second line") //Writes "Second line"
+	.close (); //Closes the writer. A flush is implicitly done.
 
 //The BufferedWriter appends content to the end of the file because append == true
-//From the end of the file:
 new BufferedWriter ("file", "utf8", true)
-	//Writes EOL (OS dependent; \r\n on Windows, otherwise \n)
-	.newLine ()
-	//Writes "Third line"
-	.write ("Third line")
-	//Closes the writer. A flush is implicitly done.
-	.close ();
+	.on ("error", function (error){
+		console.log (error);
+	})
+	
+	//From the end of the file:
+	.newLine () //Writes EOL (OS dependent; \r\n on Windows, otherwise \n)
+	.write ("Third line") //Writes "Third line"
+	.close (); //Closes the writer. A flush is implicitly done.
 	
 //The file contains:
 /*
